f09b0399b5d439a52174dc8171d8c3ab2c2062fe
Amit Verma <amit.verma@oyelabs.com>
2025-04-27 14:37:26 +0530

    added csv import

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ExpenseController;
 Route::get('/', function () {
-    return view('home');
+    return redirect('/expenses');
 });
 
 Auth::routes();
@@ -13,7 +13,7 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Route::get('expenses/export', [HomeController::class, 'exportCsv'])->name('expenses.export');
 Route::middleware(['auth'])->group(function () {
     Route::resource('expenses', ExpenseController::class);
-    Route::get('expenses/export', [ExpenseController::class, 'exportCsv'])->name('expenses.export');
+    // Route::get('expenses/export', [ExpenseController::class, 'exportCsv'])->name('expenses.export');
 });
 Route::get('/expenses/daywise/{date}', [ExpenseController::class, 'getDaywiseExpenses'])->name('expenses.daywise');
-Route::get('expenses/export', [ExpenseController::class, 'exportCsv'])->name('expenses.export');
+// Route::get('expenses/export', [ExpenseController::class, 'exportCsv'])->name('expenses.export');
